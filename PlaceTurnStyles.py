d9e98144643b9837b15480cb2dcfebb0798ff866
Samuel A. Micka <mickas37@gmail.com>
2016-02-13 12:00:10 -0700

    New comment, want to save before lunch

--- PlaceTurnStyles.py
+++ PlaceTurnStyles.py
@@ -11,7 +11,7 @@ def find_nearest_turn_style(v, graph):
         path = q.pop(0)
         vertex = path[-1]
         if vertex in graph.get_turn_styles():
-            return path
+            return path #len(path) we will ultimately want the length of the path to determine error
         for neighbors in graph.get_neighbors(vertex):
             new_path = list(path)
             new_path.append(neighbors)
