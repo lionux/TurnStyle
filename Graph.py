617e6405770563a0145e3f064325632a4f287487
Samuel A. Micka <mickas37@gmail.com>
2016-02-13 11:03:06 -0700

    Removed Driver from class file and put it into its own file

--- Graph.py
+++ Graph.py
@@ -41,23 +41,3 @@ class Graph(object):
                 print "  "+neighbor+"\n"
 
 
-def main():
-    graph_init = {
-        "a": ["b", "c", "d"],
-        "b": ["e", "a", "b"],
-        "c": ["a"],
-        "d": ["e", "a"]
-        }
-    vertex_error_init = {
-        "a" : 100,
-        "b" : 100,
-        "c" : 35,
-        "d" : 23,
-        "e" : 74
-        }
-
-    g = Graph(graph_init, vertex_error_init)
-    g.add_edge("c", "e")
-    g.print_graph()
-
-if __name__ == '__main__':main()
